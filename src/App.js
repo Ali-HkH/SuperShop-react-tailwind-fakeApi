e06cf117aacf4ef8880427d0f7185284251ce041
Ali-HkH <alikheirkhah.akk@gmail.com>
2024-10-23 15:26:39 +0330

    setup react router and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import routes from "./routes";
+import { useRoutes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Nav from "./components/Nav/Nav";
 import Superior from "./components/Superior/Superior";
 
 function App() {
+
+  const router = useRoutes(routes)
+
   return (
     <div className="App">
       <Nav />
-      <div className="w-full h-[500px]"></div>
+      {router}
       <Superior />
       <Footer />
     </div>
---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import Home from "./pages/Home/Home"
+import Blog from "./pages/Blog/Blog"
+import Cart from "./pages/Cart/Cart"
+import Category from "./pages/Category/Category"
+import Contact from "./pages/Contact/Contact"
+import About from "./pages/About/About"
+import Login from "./pages/Login/Login"
+import Register from "./pages/Register/Register"
+
+const routes = [
+    {path: "/", element:<Home /> },
+    {path: "/Blog", element:<Blog /> },
+    {path: "/Cart", element:<Cart /> },
+    {path: "/Category", element:<Category /> },
+    {path: "/Contact", element:<Contact /> },
+    {path: "/About", element:<About /> },
+    {path: "/Login", element:<Login /> },
+    {path: "/Register", element:<Register /> },
+]
+
+export default routes
