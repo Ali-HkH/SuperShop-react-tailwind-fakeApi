64e5b18045cdd6e5dc3ce50e649e546549a25537
Ali-HkH <alikheirkhah.akk@gmail.com>
2024-10-27 22:51:32 +0330

    develped trending Gallery section and galleryBox

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import OptionBox from "../../components/OptionBox/OptionBox";
+import GalleryBox from "../../components/GalleryBox/GalleryBox";
 
 function Home() {
    return (
@@ -19,7 +20,7 @@ function Home() {
                </button>
             </div>
             {/* shop options */}
-            <div className="w-[95%] bg-indigo-700 text-white absolute -bottom-96 md:-bottom-32 lg:-bottom-20 flex flex-col md:flex-row z-20 child:transition-colors child:w-full md:child:w-1/3">
+            <div className="container bg-indigo-700 text-white absolute -bottom-96 md:-bottom-32 lg:-bottom-20 flex flex-col md:flex-row z-20 child:transition-colors child:w-full md:child:w-1/3">
                <OptionBox
                   title="FREE SHIPPING"
                   subTitle="On orders over 99.00$"
@@ -34,6 +35,39 @@ function Home() {
                   title="ONLINE SUPPORT"
                   subTitle="Online support 24/7"
                   svg="support"
+               />
+            </div>
+         </div>
+         {/* Grid Gallery */}
+         <div className="container h-auto md:h-[95%] lg:h-[110vh] py-14 md:py-20 grid grid-cols-1 grid-rows-4 xs:grid-cols-2 xs:grid-rows-3 md:grid-cols-3 gap-5 lg:gap-8 child:max-h-80 xs:child:max-h-max">
+            <div className="group relative col-span-1 row-span-1 xs:row-span-2 bg-blue-400">
+               <GalleryBox
+                  img="./images/gallery/macbook.jpg"
+                  name="MACBOOK PRO"
+               />
+            </div>
+            <div className="group relative col-span-1 row-span-1 xs:row-span-3 bg-blue-400 hidden md:block">
+               <GalleryBox
+                  img="./images/gallery/watch.jpg"
+                  name="MEN'S WATCH OMEGA"
+               />
+            </div>
+            <div className="group relative col-span-1 row-span-1 bg-blue-400">
+               <GalleryBox
+                  img="./images/gallery/colores.jpg"
+                  name="COLORES APPLESFERA"
+               />
+            </div>
+            <div className="group relative col-span-1 row-span-1 xs:row-span-2 bg-blue-400">
+               <GalleryBox
+                  img="./images/gallery/macbook2.jpg"
+                  name="ACCESSORIES"
+               />
+            </div>
+            <div className="group relative col-span-1 row-span-1 bg-blue-400">
+               <GalleryBox
+                  img="./images/gallery/iphone.jpg"
+                  name="SMARTPHONE"
                />
             </div>
          </div>
